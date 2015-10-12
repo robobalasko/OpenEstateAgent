e1015e83b3b10abc658118cff1dd80dfbe05ef5a
Róbert <robo.balasko@gmail.com>
2015-10-12 14:08:30 +0200

    Implement a base API controller.

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiController extends Controller
+{
+	/**
+	 * Response array sent as a reply to a request.
+	 * 
+	 * @var array
+	 */
+	private $response = [];
+	
+	/**
+	 * Sets the HTTP code for the current request.
+	 * 
+	 * @param int $code
+	 * @throws \InvalidArgumentException
+	 */
+	public function setReponseCode($code)
+	{
+		if ( ! is_numeric($code))
+		{
+			throw new \InvalidArgumentException("Response code must be an integer, you have passed: $code");
+		}
+		
+		$this->response['code'] = $code;
+		
+		return $this;
+	}
+	
+	/**
+	 * Sets the message that is returned with the HTTP response.
+	 * 
+	 * @param string $message
+	 */
+	public function setResponseMessage($message)
+	{
+		$this->response['message'] = $message;
+		
+		return $this;
+	}
+	
+	/**
+	 * Sets the data sent back with the HTTP reponse.
+	 * 
+	 * @param mixed $data
+	 */
+	public function setReponseData($data)
+	{
+		$this->response['data'] = $data;
+		
+		return $this;
+	}
+	
+	/**
+	 * Reponds with a generic HTTP 200.
+	 * 
+	 * @param string $message
+	 * @param mixed  $data
+	 */
+	public function respondOk($message, $data = null)
+	{
+		return $this->setReponseCode(200)
+			->setResponseMessage($message)
+			->setReponseData($data);
+	}
+}
