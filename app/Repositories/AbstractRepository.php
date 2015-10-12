a7df011d4c4cc0ef1fd922f4d5275c5b33031b59
Róbert <robo.balasko@gmail.com>
2015-10-12 08:43:59 +0200

    Implement an abstract repository.

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository implements RepositoryInterface
+{
+	/**
+	 * The default model for this repository.
+	 * 
+	 * @var Model
+	 */
+	protected $model;
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \App\Repositories\RepositoryInterface::getAll()
+	 */
+	public function getAll(array $columns = ['*'])
+	{
+		return $this->model->all($columns);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \App\Repositories\RepositoryInterface::getById()
+	 */
+	public function getById($id, array $columns = ['*'])
+	{
+		return $this->model->findOrFail($id, $columns);
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @see \App\Repositories\RepositoryInterface::deleteById()
+	 */
+	public function deleteById($id)
+	{
+		$entity = $this->getById($id);
+		
+		$entity->delete();
+		
+		return $entity;
+	}
+}
